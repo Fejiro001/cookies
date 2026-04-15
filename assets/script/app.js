87ab2aafde64fe8b9ac6cb2ef0a576df39f2fa8c
Fejiro001 <aberefejiro@gmail.com>
2026-04-15 09:02:29 -0500

    Added the setCookie and getCookies functions

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,5 +1,34 @@
 "use strict";
 
-const firstDialog = document.getElementById("settings-dialog");
+const cookieDialog = document.getElementById("cookies-dialog");
+const settingsDialog = document.getElementById("settings-dialog");
 
-firstDialog.showModal();
+function setCookie(name, value, maxAge) {
+  let cookieString = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+
+  const options = {
+    path: "/",
+    SameSite: "Lax"
+  };
+  for (let option in options) {
+    cookieString += `; ${option}=${options[option]}`;
+  }
+
+  if (maxAge) {
+    cookieString += `; max-age=${maxAge}`;
+  }
+  document.cookie = cookieString;
+}
+
+function getCookies() {
+  if (document.cookie) {
+    const cookiePair = document.cookie.split(";");
+
+    cookiePair.forEach((cookie) => {
+      let [key, value] = cookie.split("=");
+      key = decodeURIComponent(key).trim();
+      value = decodeURIComponent(value).trim();
+      console.log(`Key: ${key}, Value: ${value};`);
+    });
+  }
+}
